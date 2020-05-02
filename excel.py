454ac3143162c179a57bee9efd67082c8cf1d610
N1K1TAS95 <46042802+N1K1TAS95@users.noreply.github.com>
2020-05-02 09:23:19 +0200

    Stampa dei giorni del mese su Excel

--- excel.py
+++ excel.py
@@ -1,2 +1,7 @@
-import openpyxl
+from openpyxl import load_workbook
 
+wb = load_workbook('ORE NIKITA.xlsx')
+
+ws = wb.create_sheet('PROVA')
+
+wb.save('ORE NIKITA.xlsx')
--- prova.py
+++ prova.py
@@ -1,5 +1,12 @@
 from datetime import date
 from calendar import monthrange
+from openpyxl import load_workbook
+from openpyxl.styles.borders import Border, Side
+
+thin_border = Border(left=Side(style='thin'),
+                     right=Side(style='thin'),
+                     top=Side(style='thin'),
+                     bottom=Side(style='thin'))
 
 week_days_names = {
     0: 'Lunedì',
@@ -14,8 +21,17 @@ week_days_names = {
 year = int(input("Inserisci l'anno > "))
 month = int(input('Inserisci il mese > '))
 
-days = [["%02d/%02d/%d" % (x, month, year), week_days_names[date(year, month, x).weekday()]]
+days = [[date(year, month, x).strftime("%d/%m/%Y"), week_days_names[date(year, month, x).weekday()]]
         for x in range(1, monthrange(year, month)[1] + 1)]
 
 print(days)
+
+wb = load_workbook('ORE NIKITA.xlsx')
+ws = wb.create_sheet('PROVA')
+
+for x in range(1, len(days) + 1):
+    ws.cell(1, x, days[x - 1][1]).border = thin_border
+    ws.cell(2, x, days[x - 1][0]).border = thin_border
+
+wb.save('ORE NIKITA.xlsx')
     
